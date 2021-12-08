8aff36910f932a9453af9489d2f145605b4fcc96
Christian Bouwense <bouwensec@gmail.com>
2021-12-08 14:52:08 -0500

    attempted to finish req 5.

--- src/StringCalculator/stringCalculator.js
+++ src/StringCalculator/stringCalculator.js
@@ -1,19 +1,42 @@
 const STRING_INPUT_REQUIRED_ERROR_MESSAGE = "Add required a string as input.";
-const MAX_NUMBERS = 2;
-const MAX_NUMBERS_EXCEEDED_ERROR_MESSSAGE = "Exceeded the maximum amount of input numbers.";
+const INPUT_ENDS_WITH_DELIMETER_ERROR_MESSAGE = "Cannot end the input with a delimeter.";
+
+const getDelimeterAndNumbers = (numbers) => {
+    const [ delim, nums ] = inputTokensWhenUserDefinesADelimeter = 
+        numbers.substring(2, numbers.length).split("\n");
+    return { delimeter: delim, numbers: nums };
+};
+
+const differentDelimeterUsedInNumbers = (delim, nums) => {
+    return nums.match(/[\D]/) && !nums.includes(delim)
+};
+
+let delimeters = [",", "\n"];
 
 const Add = (numbers) => {
-    if (typeof numbers !== "string") throw STRING_INPUT_REQUIRED_ERROR_MESSAGE;
+    if (typeof numbers !== "string") 
+        throw STRING_INPUT_REQUIRED_ERROR_MESSAGE;
     if (numbers === "") return 0;
 
-    const numberTokensAsStrings = numbers.split(",");
-    if (numberTokensAsStrings.length > MAX_NUMBERS) throw MAX_NUMBERS_EXCEEDED_ERROR_MESSSAGE;
+    const delimeterIsUserDefined = numbers.substring(0, 2) === "//";
+    if (delimeterIsUserDefined) {
+        const delimeterAndNumbers = getDelimeterAndNumbers(numbers);
+        delimeters = [delimeterAndNumbers.delimeter];
+        numbers = delimeterAndNumbers.numbers;
+        if (differentDelimeterUsedInNumbers(delimeters[0], numbers)) throw "";
+    }
 
+    console.log({ lastChar: numbers.slice(-1), delimeters })
+
+    if (delimeters.includes(numbers.slice(-1))) {
+        throw INPUT_ENDS_WITH_DELIMETER_ERROR_MESSAGE;
+    }
+
+    const numberTokensAsStrings = numbers.split(delimeters[0]);
     const numberIntTokens = numberTokensAsStrings.map(n => parseInt(n));
     return numberIntTokens.reduce((prev, curr) => prev + curr, 0);
 };
 
 module.exports = {
     Add,
-    STRING_INPUT_REQUIRED_ERROR_MESSAGE
 }
--- src/StringCalculator/stringCalculator.test.js
+++ src/StringCalculator/stringCalculator.test.js
@@ -1,6 +1,6 @@
 const { 
     Add,
-    MAX_NUMBERS_EXCEEDED_ERROR_MESSSAGE,
+    INPUT_ENDS_WITH_SEPARATOR_ERROR_MESSAGE,
     STRING_INPUT_REQUIRED_ERROR_MESSAGE,  
 } = require("./stringCalculator");
 
@@ -31,23 +31,58 @@ describe("Add", () => {
         });
     });
 
-    describe("when the input is more than two numbers", () => {
-        it("should throw an error", () => {
-            expect(() => Add("0,1,2"))
-                .toThrow(MAX_NUMBERS_EXCEEDED_ERROR_MESSSAGE);
-            expect(() => Add("0,1,2,3"))
-                .toThrow(MAX_NUMBERS_EXCEEDED_ERROR_MESSSAGE);
-            expect(() => Add("0,1,2,3,4,5,6,7,8,9"))
-                .toThrow(MAX_NUMBERS_EXCEEDED_ERROR_MESSSAGE);
-        });
-    });
-
     describe("when the input is two numbers", () => {
         it("should return their sum", () => {
             expect(Add("0,1")).toBe(1);
             expect(Add("0,10")).toBe(10);
             expect(Add("1,99")).toBe(100);
             expect(Add("-99,99")).toBe(0);
+        });
+    });
+
+    describe("when the input is more than two numbers", () => {
+        it("should return their sum", () => {
+            expect(Add("0,1,2")).toBe(3);
+            expect(Add("0,1,2,3,4")).toBe(10);
+        });
+    });
+
+    describe("when the input numbers are separated by a newline", () => {
+        it("should return their sum", () => {
+            expect(Add("0\n1")).toBe(1);
+        });
+    });
+
+    describe("when the input numbers are separated by commas and newlines", () => {
+        it("should return their sum", () => {
+            expect(Add("0\n1,2\n3")).toBe(6);
+        });
+    });
+
+    describe("when there is a separator at the end of the input", () => {
+        it("should throw an error", () => {
+            expect(() => Add(",")).toThrow(INPUT_ENDS_WITH_SEPARATOR_ERROR_MESSAGE);
+            expect(() => Add("\n")).toThrow(INPUT_ENDS_WITH_SEPARATOR_ERROR_MESSAGE);
+            expect(() => Add("0,")).toThrow(INPUT_ENDS_WITH_SEPARATOR_ERROR_MESSAGE);
+            expect(() => Add("0\n")).toThrow(INPUT_ENDS_WITH_SEPARATOR_ERROR_MESSAGE);
+            expect(() => Add("0,1,")).toThrow(INPUT_ENDS_WITH_SEPARATOR_ERROR_MESSAGE);
+            expect(() => Add("0,1\n")).toThrow(INPUT_ENDS_WITH_SEPARATOR_ERROR_MESSAGE);
+        })
+    });
+
+    describe("when a delimiter is defined in the input", () => {
+        it("should throw an error when that delimeter is at the end of the input", () => {
+            expect(() => Add("//;\n0;1;")).toThrow();
+        });
+
+        it("should return the sum of numbers", () => {
+            expect(Add("//;\n0;1")).toBe(1);
+            expect(Add("//|\n0|1")).toBe(1);
+            expect(Add("//|\n0|1|2|99")).toBe(102);
+        });
+        
+        it("should throw an error if another delimiter is used", () => {
+            expect(() => Add("//;\n1,3")).toThrow();
         });
     });
 });
