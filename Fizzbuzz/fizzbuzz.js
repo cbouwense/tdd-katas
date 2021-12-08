d2519c90704b77f4e472332bf60a27752f4651ae
Christian Bouwense <bouwensec@gmail.com>
2021-12-08 12:27:49 -0500

    fully implemented fizzbuzz.

--- Fizzbuzz/fizzbuzz.js
+++ Fizzbuzz/fizzbuzz.js
@@ -1,9 +1,16 @@
-const fizzBuzz = () => {
-    return "fizzBuzz";
+const numberRequiredErrorMessage = "fizzBuzz requires a number as input.";
+
+const fizzBuzz = (n) => {
+    if (typeof n !== "number") 
+        throw numberRequiredErrorMessage;
+    
+    if (n % 15 === 0) return "fizzbuzz";
+    if (n % 3 === 0) return "fizz";
+    if (n % 5 === 0) return "buzz";
+    return `${n}`;
 };
 
-fizzBuzz();
-
 module.exports = {
-    fizzBuzz
+    fizzBuzz,
+    numberRequiredErrorMessage
 };
--- Fizzbuzz/fizzbuzz.test.js
+++ Fizzbuzz/fizzbuzz.test.js
@@ -1,7 +1,45 @@
-const { fizzBuzz } = require("./fizzbuzz");
+const { fizzBuzz, numberRequiredErrorMessage } = require("./fizzbuzz");
 
 describe("fizzBuzz", () => {   
+    it("should throw error if not given an argument", () => {
+        expect(() => fizzBuzz()).toThrow(numberRequiredErrorMessage);
+    });
+
+    it("should throw error if not given a number", () => {
+        expect(() => fizzBuzz("foo")).toThrow(numberRequiredErrorMessage);
+    });
+    
     it("should return a string", () => {
-        expect(typeof fizzBuzz()).toBe("string");
+        expect(typeof fizzBuzz(0)).toBe("string");
+    });
+
+    it("should return the number given", () => {
+        expect(fizzBuzz(1)).toBe("1");
+        expect(fizzBuzz(-1)).toBe("-1");
+        expect(fizzBuzz(101)).toBe("101");
+    });
+
+    describe("when the number given is a multiple of three", () => {
+        it("should return \"fizz\"", () => {
+            expect(fizzBuzz(3)).toBe("fizz");
+            expect(fizzBuzz(6)).toBe("fizz");
+            expect(fizzBuzz(99)).toBe("fizz");
+        });
+    });
+
+    describe("when the number given is a multiple of five", () => {
+        it("should return \"buzz\"", () => {
+            expect(fizzBuzz(5)).toBe("buzz");
+            expect(fizzBuzz(10)).toBe("buzz");
+            expect(fizzBuzz(100)).toBe("buzz");
+        });
+    });
+
+    describe("when the number given is a multiple of both three and five", () => {
+        it("should return \"fizzbuzz\"", () => {
+            expect(fizzBuzz(0)).toBe("fizzbuzz");
+            expect(fizzBuzz(15)).toBe("fizzbuzz");
+            expect(fizzBuzz(-15)).toBe("fizzbuzz");
+        });
     });
 });
